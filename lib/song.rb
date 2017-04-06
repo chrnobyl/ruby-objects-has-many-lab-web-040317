74959dc637964ee9cbcef5ff09b1cd88f6900e95
github username <github email address>
2017-04-06 13:43:21 -0400

    Done

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+require 'artist.rb'
+
+class Song
+
+	attr_accessor :name, :artist
+
+	def initialize(name)
+		@name = name
+	end
+
+	def artist_name
+		if self.artist
+			self.artist.name
+		else
+			nil
+		end
+		
+	end
+
+end
